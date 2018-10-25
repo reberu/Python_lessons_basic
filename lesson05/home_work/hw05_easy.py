2b12cf3722adac889ee054ba7078c3802f9af543
reberu <redlightexit@gmail.com>
2018-10-25 21:19:19 +0900

    hw05_easy.py и hw05_normal.py выполнены.

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,34 @@
+import os
+import sys
+import shutil
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+# Создание папок
+for i in range(1, 9):
+    dirname = 'dir_' + str(i)
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+# Удаление папок
+path = os.getcwd()
+dirs = os.listdir(path)
+for i in dirs:
+    if "dir_" in i:
+        os.rmdir(i)
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+path = os.getcwd()
+subfolders = [f.name for f in os.scandir(path) if f.is_dir()]
+for i in subfolders:
+    print(i)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+path = os.getcwd()
+dst = str(path) + r'\thisiscopy.py'
+shutil.copyfile(sys.argv[0], dst)
+print(dst)
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -9,6 +9,58 @@
 # При выборе пунктов 1, 3, 4 программа запрашивает название папки
 # и выводит результат действия: "Успешно создано/удалено/перешел",
 # "Невозможно создать/удалить/перейти"
+import os
+
+while True:
+    print('1. Перейти в папку')
+    print('2. Просмотреть содержимое папки')
+    print('3. Удалить папку')
+    print('4. Создать папку')
+    print('5. Завершить программу')
+    choice = input('Выберите нужную операцию: ')
+    path = os.getcwd()
+    if choice == '5':
+        print('Всего хорошего!')
+        break
+
+    elif choice == '1':
+        dirname = input('Введите название папки: ')
+        fullpath = str(path) + r"\\" + dirname
+        if os.path.exists(fullpath):
+            os.chdir(fullpath)
+            print('Успешно перешли в папку. Текущая директория: ' + os.getcwd())
+            break
+        else:
+            print('Невозможно перейти в папку')
+            break
+
+    elif choice == '2':
+        print('Содержимое папки:')
+        for i in os.listdir(os.getcwd()):
+            print(i)
+        break
+
+    elif choice == '3':
+        dirname = input('Введите название папки: ')
+        fullpath = str(path) + r"\\" + dirname
+        if os.path.exists(fullpath):
+            os.rmdir(fullpath)
+            print('Успешно удалена папка ' + dirname)
+            break
+        else:
+            print('Невозможно удалить папку')
+            break
+
+    elif choice == '4':
+        dirname = input('Введите название папки: ')
+        fullpath = str(path) + r"\\" + dirname
+        if not os.path.exists(fullpath):
+            os.mkdir(fullpath)
+            print('Успешно создана папка ' + dirname)
+            break
+        else:
+            print('Невозможно создать папку')
+            break
 
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
